5048ed8572467fa0ef72b4430fcbfa3bab8c3412
YHKIM\kvoya <kvoyant@naver.com>
2024-05-21 17:32:59 +0900

    Nested routes,  outlet - http://localhost:3000/event/ - http://localhost:3000/event/two

--- src/App.js
+++ src/App.js
@@ -28,12 +28,27 @@ function App() {
           <Route path="member" element={<div>멤버들</div>} />
           <Route path="location" element={<div>회사위치</div>} />
         </Route>
-        
+
         <Route path="*" element={<div>없는페이지임</div>} />
+
+        <Route path="/event" element={<EventPage />}>
+          <Route path="one" element={<p>첫 주문시 양배추즙 서비스</p>}></Route>
+          <Route path="two" element={<p>생일기념 쿠폰받기</p>}></Route>
+        </Route>
+        
       </Routes>
 
     </>
   );
+}
+
+function EventPage() {
+  return (
+    <div>
+      <h4>오늘의 이벤트</h4>
+      <Outlet></Outlet>
+    </div>
+  )
 }
 
 function About() {
