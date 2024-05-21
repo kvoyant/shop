095f01cdb7a727f7c943c6c0d72bd2a160fa664a
YHKIM\kvoya <kvoyant@naver.com>
2024-05-21 16:53:15 +0900

    라우팅 react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,31 +1,38 @@
-import { Container, Nav, Navbar } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-// import bg from 'https://codingapple1.github.io/shop/shoes1.jpg';
-import bg from './bg.png';
 import { useState } from 'react';
+import { Link, Route, Routes } from 'react-router-dom';
 import data from './data';
 
 function App() {
   let [shoes] = useState(data);
   return (
-    <div className="App">
-      <div className="container">
-        <div className="row">
-          <Card shoes={shoes[0]} i={1}/>
-          <Card shoes={shoes[1]} i={2} />
-          <Card shoes={shoes[2]} i={3} />
-        </div>
-      </div>
-    </div>
+    <Routes>
+      <Route path="/" element={
+        <>
+          <div className='main-bg'></div>
+          <div className='container'>
+            <div className='row'>
+              {shoes.map((a, i) => {
+                return <Card shoes={shoes[i]} i={i + 1} key={i}></Card>
+              })}
+            </div>
+          </div>
+        </>
+      } />
+      <Route path="/detail" element={<div>상세페이지임</div>} />
+      <Route path="/about" element={<div>어바웃페이지임</div>} />
+    </Routes>
   );
 }
 
 function Card(props) {
   return (
+    
     <div className="col-md-4">
-      <img src={"https://codingapple1.github.io/shop/shoes"+ props.i +".jpg"} width="80%" />
-      <h4>{ props.shoes.title }</h4>
-      <p>{ props.shoes.price }</p>
+      <img src={"https://codingapple1.github.io/shop/shoes" + props.i + ".jpg"} width="80%" />
+      <h4>{props.shoes.title}</h4>
+      <p>{props.shoes.price}</p>
+      {/* <Link to="/">홈</Link> */}
+
     </div>
   )
 }
