f00a73cfa1dff486284344dfb46c54dbf83377bb
YHKIM\kvoya <kvoyant@naver.com>
2024-05-21 15:54:47 +0900

    Card 컴포넌트 분리

--- src/App.js
+++ src/App.js
@@ -8,49 +8,26 @@ import data from './data';
 function App() {
   let [shoes] = useState(data);
   return (
-    <>
-      {/* <div>
-        <div className="main-bg" style={{ backgroundImage: 'url(' + bg + ')' }}></div>
-      </div> */}
-
-      <div className="App">
-        <Navbar bg="dark" variant="dark">
-          <Container>
-            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#features">Features</Nav.Link>
-              <Nav.Link href="#pricing">Pricing</Nav.Link>
-            </Nav>
-          </Container>
-        </Navbar>
-
-        <div className="container">
-          <div className="row">
-            <div className="col-md-4">
-              <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%" />
-              <h4>{ shoes[0].title }</h4>
-              <p>{ shoes[0].price }</p>
-            </div>
-            <div className="col-md-4">
-              <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%" />
-              <h4>상품명</h4>
-              <p>상품정보</p>
-            </div>
-            <div className="col-md-4">
-              <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%" />
-              <h4>상품명</h4>
-              <p>상품정보</p>
-            </div>
-          </div>
+    <div className="App">
+      <div className="container">
+        <div className="row">
+          <Card shoes={shoes[0]} i={1}/>
+          <Card shoes={shoes[1]} i={2} />
+          <Card shoes={shoes[2]} i={3} />
         </div>
       </div>
-
-
-    </>
-
-
+    </div>
   );
+}
+
+function Card(props) {
+  return (
+    <div className="col-md-4">
+      <img src={"https://codingapple1.github.io/shop/shoes"+ props.i +".jpg"} width="80%" />
+      <h4>{ props.shoes.title }</h4>
+      <p>{ props.shoes.price }</p>
+    </div>
+  )
 }
 
 export default App;
