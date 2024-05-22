4ecc3843d6fc4439e962063669cb3069fbd935b7
YHKIM\kvoya <kvoyant@naver.com>
2024-05-22 09:23:02 +0900

    url 파라미터로 상세페이지 만들기 - arrow function - find(), parseInt()

--- src/Detail.js
+++ src/Detail.js
@@ -2,7 +2,17 @@ import { useParams } from "react-router-dom";
 
 const Detail = (props) => {
     let {id} = useParams();
+    
+    //arrow function
+    let 찾은상품 = props.shoes.find((x) => x.id === parseInt(id));
+    
+    //그냥 function
+    let 찾은상품2 = props.shoes.find(function(x){
+        return x.id === id;
+    });
+
     console.log(id)
+    console.log('찾은상품 :',찾은상품)
     
     return (
         <div className="container">
@@ -11,9 +21,9 @@ const Detail = (props) => {
                     <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="30%" />
                 </div>
                 <div className="col-md-6 mt-4">
-                    <h4 className="pt-5">{props.shoes[id].title}</h4>
-                    <p>{props.shoes[id].content}</p>
-                    <p>{props.shoes[id].price}원</p>
+                    <h4 className="pt-5">{찾은상품.title}</h4>
+                    <p>{찾은상품.content}</p>
+                    <p>{찾은상품.price}원</p>
                     <button className="btn btn-danger">주문하기</button>
                 </div>
             </div>
