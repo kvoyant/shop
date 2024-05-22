aa9fc4db99312ca2a41f227e65315e6184a0a48e
YHKIM\kvoya <kvoyant@naver.com>
2024-05-22 17:52:17 +0900

    css fade 효과 주기 - setTimeout, useEffect, useState

--- src/Detail.js
+++ src/Detail.js
@@ -1,12 +1,44 @@
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { Nav } from "react-bootstrap";
 
 const Detail = (props) => {
     let [shoes, setShoes] = useState('');
+    let [탭, 탭변경] = useState(0);
+    let [fade, setFade] = useState('');
+
+    useEffect(() => {
+        setTimeout(() => {setFade('end')}, 1000);
+        return () => {
+            setFade('');
+        }
+        // setFade('end');
+    }, [탭])
+
+    function TabContent(props) {
+        console.log(props.탭)
+        // if (props.탭 === 0) {
+        //     return <div>내용0</div>
+        // }
+        // if (props.탭 === 1) {
+        //     return <div>내용1</div>
+        // }
+        // if (props.탭 === 2) {
+        //     return <div>내용2</div>
+        // }
+
+        return (
+            <div className={'start ' + fade}>
+                {[<div>내용0</div>, <div>내용1</div>, <div>내용2</div>][props.탭]}
+            </div>
+        )
+    }
 
     return (
+        <>
+        <TabContent 탭={탭} />
         <div>
-            <button onClick={() => {
+            {/* <button onClick={() => {
                 axios.get('https://codingapple1.github.io/shop/data2.json').then((결과) => {
                     console.log('결과.data :', 결과.data)
                     let copy = [...결과.data, ...결과.data];//6개
@@ -20,12 +52,29 @@ const Detail = (props) => {
 
             <button onClick={() => {
                 fetch('https://codingapple1.github.io/shop/data2.json')
-                .then(결과 => 결과.json())//json  변환
-                .then((결과) => {
-                    console.log('fetch :', 결과) }
-                )
-            }}>fetch 서버통신</button>
+                    .then(결과 => 결과.json())//json  변환
+                    .then((결과) => {
+                        console.log('fetch :', 결과)
+                    }
+                    )
+            }}>fetch 서버통신</button> */}
+
+            <Nav variant="tabs" defaultActiveKey="link0">
+                <Nav.Item>
+                    <Nav.Link onClick={() => { 탭변경(0) }} eventKey="link0">버튼0</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link onClick={() => { 탭변경(1) }} eventKey="link1">버튼1</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link onClick={() => { 탭변경(2) }} eventKey="link2">버튼2</Nav.Link>
+                </Nav.Item>
+            </Nav>
+            <div>내용0</div>
+            <div>내용1</div>
+            <div>내용2</div>
         </div>
+        </>
     );
 };
 
