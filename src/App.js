e26ead61df832689abe0ea31895b0fe257b58080
YHKIM\kvoya <kvoyant@naver.com>
2024-05-24 17:27:28 +0900

    성능개선 1 : 개발자도구 & lazy import - lazy import : 첫 페이지 로딩속도를 향상

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-import { useState } from 'react';
+import { useState, lazy } from 'react';
 import { Link, Outlet, Route, Routes, useNavigate } from 'react-router-dom';
 import data from './data';
-import Detail from './Detail';
-import Cart from './Cart';
+// import Detail from './Detail';
+// import Cart from './Cart';
 import { useQuery } from 'react-query';
 import axios from 'axios';
+
+const Detail = lazy( () => import('./Detail.js') )
+const Cart = lazy( () => import('./Cart.js') )
+/**
+ * 
+ lazy import
+
+리액트 코드 다 짰으면 npm run build 입력해서
+여러분이 짰던 이상한 코드들을 역사와 전통의 html css js 파일로 변환해야합니다.  
+근데 리액트로 만드는 Single Page Application의 특징은 html, js 파일이 하나만 생성됩니다. 
+그 안에 지금까지 만든 App / Detail / Cart 모든 내용이 들어있어서 파일사이즈가 좀 큽니다. 
+원래 그래서 리액트 사이트들은 첫 페이지 로딩속도가 매우 느릴 수 있습니다.  
+
+그게 싫다면 js 파일을 잘게 쪼개면 됩니다.
+쪼개는 방법은 import 문법을 약간 고치면 되는데  
+지금 메인페이지 보면 Detail, Cart를 import 해서 쓰고있습니다.
+근데 잘 생각해보면 Detail, Cart 컴포넌트는 메인페이지에서 전혀 보이지 않고 있기 때문에
+이런 컴포넌트들은 이런 문법으로 import 해놓으면 좋습니다. 
+
+lazy 문법으로도 똑같이 import가 가능한데 이 경우엔
+"Detail 컴포넌트가 필요해지면 import 해주세요" 라는 뜻이 됩니다. 
+그리고 이렇게 해놓으면 Detail 컴포넌트 내용을 다른 js 파일로 쪼개줍니다.
+그래서 첫 페이지 로딩속도를 향상시킬 수 있습니다.
+필요할 것 같으면 씁시다. 
+
+*/
 
 function App() {
   let [shoes] = useState(data);
