ee4cfccb56d3322ab2594f2983dfc8409a64b5cd
YHKIM\kvoya <kvoyant@naver.com>
2024-05-27 13:26:50 +0900

    마무리 참고 사이트

--- src/data.js
+++ src/data.js
@@ -20,5 +20,18 @@ let data = [
       price : 130000
     }
   ];
+
+  /**!SECTION
+   * 
+개발욕구를 자극하기 위한 리액트 사이트 몇가지 보여드리면 
+
+리액트로 만든 테트리스 https://chvin.github.io/react-tetris/ (spacebar, 방향키 이용)
+간단한 영화정보 검색기 https://skempin.github.io/reactjs-tmdb-app/
+파일 버전관리 툴인 git 문법 배우기 앱 https://learngitbranching.js.org/
+iOS 스타일 계산기 https://codepen.io/mjijackson/full/xOzyGX
+가짜 주식 트레이딩 앱 http://web-demo.adaptivecluster.com/
+캘린더 UI http://clauderic.github.io/react-infinite-calendar/
+스케치패드 http://svrcekmichal.github.io/react-sketchpad/ 
+솔리테르 Solitaire 게임 http://pl12133.github.io/react-solitaire/   */
   
   export default data;
